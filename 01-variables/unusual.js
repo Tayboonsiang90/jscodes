ac0ab9e15548182848239e53dacafdec8f09937b
Boon Siang <tayboonsiang90@gmail.com>
2022-01-26 03:29:47 +0000

    unusual.js

---
+++ 01-variables/unusual.js
@@ -0,0 +1,8 @@
+let something;
+console.log("something= ", something) //(undefined)
+
+let s = something + 10;
+console.log("s =", s) //(undefined plus number = NaN)
+
+let v = s + 20;
+console.log("v =", v)//(NaN plus number = NaN)
---
+++ 01-variables/variables.js
@@ -0,0 +1,13 @@
+
+
+let secretOfLife = 42;
+console.log(secretOfLife);
+
+//. implies the log function is one of the functions in the console container (under objects)
+
+//you can only "let" a variable once normally
+//let secretOfLife = 43; ERROR
+
+//reassign variables
+secretOfLife = 43;
+console.log("secretOfLife =", secretOfLife);
