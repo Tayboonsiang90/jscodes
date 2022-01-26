79f78b11fe2bb07bd6c527db43db59d2dfed8a56
Boon Siang <tayboonsiang90@gmail.com>
2022-01-26 05:39:31 +0000

    added bmi conversion example

---
+++ 01-variables/bmi.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+
+let weight = parseFloat(prompt("Please enter your weight: "));
+let height = parseFloat(prompt("Please enter your height: "));
+let bmi = weight / height ** 2; 
+
+console.log(`Your BMI is ${bmi}.`)
---
+++ 01-variables/conversion.js
@@ -0,0 +1,24 @@
+console.log("1"+1); //JS will automatically convert 1 to string
+
+console.log("1"-1); //JS will automatically convert the string 1 to integer
+
+
+//String to numbers
+let number = parseInt("42");//Integer
+let pi = parseFloat("3.14");//Float
+let d = Number("2");//Integer
+let f = Number("3.14");//Float
+
+//Convert to string
+let secretOfLife = 42 + ""; //Fastest way
+
+
+
+
+
+
+
+
+
+
+
